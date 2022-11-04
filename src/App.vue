01ba53a6e9ca3ee6e9935679fd0fe3d79e6a7a63
Maksim <kyngfy-panda@yandex.ru>
2022-11-04 11:46:17 +0300

    add empty component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,63 @@
 <template>
-  <div></div>
+  <div class="q-pa-md row q-gutter-md">
+    <q-card>
+      <q-card-section style="padding: 8px">
+        <QueueChart
+            id="1"
+            :margin="{top: 0, right: 0, bottom: 0, left: 0}"
+            :width="width * 0.5"
+            :height="height * 0.5"
+        ></QueueChart>
+      </q-card-section>
+    </q-card>
+    <q-card>
+      <q-card-section style="padding: 8px">
+        <QueueChart
+            id="2"
+            :margin="{top: 0, right: 0, bottom: 0, left: 0}"
+            :width="width * 0.5"
+            :height="height * 0.5"
+        ></QueueChart>
+      </q-card-section>
+    </q-card>
+    <q-card>
+      <q-card-section style="padding: 8px">
+        <QueueChart
+            id="3"
+            :margin="{top: 0, right: 0, bottom: 0, left: 0}"
+            :width="width * 0.5"
+            :height="height * 0.5"
+        ></QueueChart>
+      </q-card-section>
+    </q-card>
+    <q-card>
+      <q-card-section style="padding: 8px">
+        <QueueChart
+            id="4"
+            :margin="{top: 0, right: 0, bottom: 0, left: 0}"
+            :width="width * 0.5"
+            :height="height * 0.5"
+        ></QueueChart>
+      </q-card-section>
+    </q-card>
+  </div>
 </template>
 
 <script>
 
+import QueueChart from "@/components/QueueChart";
+
 export default {
   name: 'App',
   components: {
-
-  }
+    QueueChart
+  },
+  data() {
+    return {
+      width: window.innerWidth,
+      height: window.innerHeight
+    }
+  },
 }
 </script>
 
@@ -19,6 +68,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/QueueChart.vue
@@ -0,0 +1,47 @@
+<template>
+  <div :id="containerID.replace('#', '')"/>
+</template>
+
+
+<script>
+import * as d3 from 'd3'
+
+const componentName = 'QueueChart'
+
+export default {
+  name: componentName,
+  data() {
+    return {
+      rootGroup: Selection,
+    }
+  },
+  props: {
+    id: {type: String, required: true},
+    margin: {type: Object, required: true},
+    width: {type: Number, required: true},
+    height: {type: Number, required: true},
+  },
+  computed: {
+    containerID() {
+      return `#${componentName}Container${this.id}`
+    }
+  },
+  mounted() {
+    this.rootGroup = addRootGroup(this.containerID, this.width, this.height, this.margin)
+  },
+}
+
+function addRootGroup(containerID, width, height, margin) {
+  return d3.select(containerID)
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height)
+      .append('g')
+      .attr('transform', `translate(${margin.left}, ${margin.top})`)
+}
+
+</script>
+
+<style scoped>
+
+</style>
